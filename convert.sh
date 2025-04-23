821e5d3d9fb4f1291db1faa7f50a8a3e30648c72
Thomas Wilbur <twilbur@adaclare.com>
2025-04-23 05:28:55 -0500

    Update convert.sh

--- convert.sh
+++ convert.sh
@@ -43,6 +43,15 @@ echo "Installing LXDE (minimal)..."
 apt install --no-install-recommends lxde sudo git -y
 apt install xinit xserver-xorg plymouth -y 
 apt install python3-venv python3-pip -y
+sudo install -d -m 0755 /etc/apt/keyrings
+wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
+echo '
+Package: *
+Pin: origin packages.mozilla.org
+Pin-Priority: 1000
+' | sudo tee /etc/apt/preferences.d/mozilla
+sudo apt-get update && sudo apt-get install firefox
 
 # Enable autologin for athenaos
 echo "Setting up autologin for 'athenaos'..."
