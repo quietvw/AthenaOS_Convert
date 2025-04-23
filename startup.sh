9129154791427bb7e980c9730d15258e84e9d61d
Thomas Wilbur <twilbur@adaclare.com>
2025-04-23 04:39:59 -0500

    Create startup.sh

---
+++ startup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sleep 5
+RES="800 400 60"
+DISP=$(xrandr | grep " connected" | awk '{print $1}')
+MODELINE=$(cvt $RES | grep -oP 'Modeline \K.*')
+MODERES=$(echo $MODELINE | cut -d' ' -f1)
+
+xrandr --newmode $MODELINE && \
+xrandr --addmode $DISP $MODERES && \
+xrandr --output $DISP --mode $MODERES
+
