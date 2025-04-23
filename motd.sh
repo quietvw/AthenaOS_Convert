995aa27be859625c2cf254d110cb8feedb423ae4
Thomas Wilbur <twilbur@adaclare.com>
2025-04-23 13:50:59 -0500

    Create motd.sh

---
+++ motd.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cat <<'EOF'
+#                                                          
+# Welcome to the Adaclare AthenaOS Appliance               
+#                                                          
+# AthenaOS - The Intelligence Operating System             
+#                                                          
+EOF
+
+
+cat <<'EOF'
+# Manage your appliance via the AthenaOS Console        
+#
+EOF
